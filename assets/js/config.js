d1ba2549804b301b7faf4d2de9b25853dfbabe50
1oldguyinaz-glitch <1oldguyinaz@gmail.com>
2026-07-25 12:44:42 -0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.AXIS_CONFIG = {
-  // Paste the deployed Google Apps Script Web App URL here.
+  https://script.google.com/macros/s/AKfycbyQJ9wpCA3gwWvg4YhXu9E6Kb8j0sn8h8MEXUny_Qw8rX152dOJOjR9_rIde5cYHeahvQ/exec
   formEndpoint: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
   thankYouPage: "thank-you.html",
   businessName: "Axis",
