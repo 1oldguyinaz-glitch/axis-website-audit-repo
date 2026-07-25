580622f843372cf9f7f78ae8f2705938dd068f8b
1oldguyinaz-glitch <1oldguyinaz@gmail.com>
2026-07-25 12:49:37 -0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,6 @@
 window.AXIS_CONFIG = {
-  https://script.google.com/macros/s/AKfycbyQJ9wpCA3gwWvg4YhXu9E6Kb8j0sn8h8MEXUny_Qw8rX152dOJOjR9_rIde5cYHeahvQ/exec
-  formEndpoint: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
+  formEndpoint: "https://script.google.com/macros/s/AKfycbyQJ9wpCA3gwWvg4YhXu9E6Kb8j0sn8h8MEXUny_Qw8rX152dOJOjR9_rIde5cYHeahvQ/exec",
   thankYouPage: "thank-you.html",
   businessName: "Axis",
   auditDeliveryWindow: "within two business days",
-  contactEmail: "REPLACE_WITH_YOUR_EMAIL@example.com"
 };
